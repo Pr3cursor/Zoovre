003c3c837f980d17f70426fc1b7df8a5bb7a1a9a
surely nicht akun <>
2026-02-01 15:34:12 +0100

    roll +  anim

--- zoovre/scripts/player.gd
+++ zoovre/scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody3D
 
 @export var move_speed = 5.0
 @export var acceleration = 20.0
+
+@onready var anim : AnimationPlayer = $raccoon.get_node("AnimationPlayer")
+
 const JUMP_VELOCITY = 4.5
 var cam: Camera3D
 var cam_input_direction := Vector2.ZERO
@@ -27,15 +30,26 @@ func _physics_process(delta: float) -> void:
 	var move_direction := forward*raw_input.y + right * raw_input.x
 	move_direction.y = 0.0
 	move_direction = move_direction.normalized()
-	look_at(move_direction)
-
+	look_at(global_position + move_direction, Vector3.UP)
 	velocity = velocity.move_toward(move_direction*move_speed,acceleration * delta)
 	move_and_slide()
-	if velocity.length() > 0:
+	
+	if velocity.length() > 0 and !anim.assigned_animation == "barrel_roll":
 		get_node("raccoon/AnimationPlayer").play("walk_animation")
-	elif velocity.length() <= 0:
+	elif velocity.length() <= 0 or anim.assigned_animation == "barrel_roll":
 		get_node("raccoon/AnimationPlayer").clear_queue()
+	
+	
+func _input(event):
+	if event.is_action_pressed("barrel_roll"):
+		barrel_roll()
 	
 func game_over():
 		print("End")
 		Gamemanager.reload_level()
+		
+func barrel_roll():
+	anim.play("barrel_roll")
+	move_speed = 50
+	await get_tree().create_timer(0.5).timeout
+	move_speed = 5.0

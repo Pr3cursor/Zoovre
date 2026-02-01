04e5b6466f8f55f4c72693f0296e1e57a240ef0e
surely nicht akun <>
2026-02-01 16:54:39 +0100

    pick_up animation

--- zoovre/scripts/pick_up.gd
+++ zoovre/scripts/pick_up.gd
@@ -1,14 +1,17 @@
 extends Area3D
 
 var player_in_range = false
+@onready var animation: AnimationPlayer =  Gamemanager.player.get_node("raccoon").get_node("AnimationPlayer")
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		player_in_range = !player_in_range
-		print(player_in_range)
+		#print(player_in_range)
 
 func pick_up():
+	animation.play("caught")
 	owner.queue_free()
+	
 	
 	
 func _physics_process(delta: float) -> void:

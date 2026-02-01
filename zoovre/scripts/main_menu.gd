f939c6e88d97a8ac72b659660c55bd0ca952b5c3
SebastianMegies <sebastian.megies@uni-bayreuth.de>
2026-02-01 15:04:17 +0100

    Shit UI

---
+++ zoovre/scripts/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/testscene.tscn")
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()

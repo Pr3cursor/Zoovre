de3c8ad8f06cc2e9aa3fa009767630260d3bdbf3
surely nicht akun <>
2026-04-18 19:08:33 +0200

    prog bar

---
+++ zoovre/scenes/progress_bar.gd
@@ -0,0 +1,4 @@
+extends ProgressBar
+
+func _ready() -> void:
+	Gamemanager.prog_bar = self
--- zoovre/scripts/gamemanager.gd
+++ zoovre/scripts/gamemanager.gd
@@ -5,3 +5,4 @@ var auto_cur_cam_id: int = 1
 var cur_cam_node: SurveillanceCam = null
 var caught: bool = false
 var reset_cam: SurveillanceCam = null
+var prog_bar: ProgressBar = null

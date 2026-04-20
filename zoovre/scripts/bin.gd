d7069387153439a96635002c7b244d84235a945a
akunbop <albertscheuerpflug@gmail.com>
2026-04-21 01:01:27 +0200

    bin anim v2

--- zoovre/scripts/bin.gd
+++ zoovre/scripts/bin.gd
@@ -7,6 +7,10 @@ extends Node3D
 var player_inside: bool = false
 var out_pos: Vector3
 
+signal player_bin_change
+
+func _ready():
+	player_bin_change.connect(Gamemanager.player._on_player_in_bin)
 func _on_area_3d_body_entered(body: Node3D):
 	if body.is_in_group("player"):
 		player_inside = true
@@ -18,31 +22,23 @@ func _on_area_3d_body_exited(body: Node3D) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and player_inside:
-		Gamemanager.player.can_move = false
 		player_inside = false
 		label.text = "press e to get out"
-		Gamemanager.player.is_in_bin = true
 		Gamemanager.player.look_at(marker_3d.position)
+		emit_signal("player_bin_change")
 		await get_tree().create_timer(2).timeout
 		animation_player.play("expand")
 		out_pos = Gamemanager.player.global_position
 		Gamemanager.player.global_position = marker_3d.global_position
-		Gamemanager.player.scale *= 0.1
-
+		#Gamemanager.player.scale *= 0.1
 		
-		
-	elif event.is_action_pressed("interact") and !player_inside and !Gamemanager.player.can_move:
-		#var dir = Gamemanager.player.global_position.normalized()
-		#Gamemanager.player.global_position = out_pos + dir
-		#Gamemanager.player.look_at(out_pos)
-		Gamemanager.player.visible = false
+	elif event.is_action_pressed("interact") and Gamemanager.player.State.IN_BIN:
+		emit_signal("player_bin_change")
 		Gamemanager.player.position = out_pos
-		Gamemanager.player.is_in_bin = false
 		await get_tree().create_timer(0.7).timeout
 		Gamemanager.player.visible = true
-		Gamemanager.player.scale *= 10
-		Gamemanager.player.exited_bin = true
+		#Gamemanager.player.scale *= 10
 		await get_tree().create_timer(1).timeout
-		Gamemanager.player.can_move = true
 		label.text = "press e to hide"
 		animation_player.play("expand")
+		print("test")
--- zoovre/scripts/player.gd
+++ zoovre/scripts/player.gd
@@ -5,7 +5,7 @@ extends CharacterBody3D
 @export var acceleration = 20.0
 @onready var animation_tree: AnimationTree = $AnimationTree
 
-@onready var anim : AnimationPlayer = $raccoon.get_node("AnimationPlayer")
+@onready var anim : AnimationPlayer = $raccoon_3/AnimationPlayer
 
 const JUMP_VELOCITY = 4.5
 var cam: Camera3D
@@ -13,13 +13,45 @@ var cam_input_direction := Vector2.ZERO
 const EPSILON = 0.01
 
 var can_move: bool = true
-var is_in_bin: bool = false
-var exited_bin: bool = false
+
+enum State {IDLE, MOVE, MOVE_IN_BIN, IN_BIN, MOVE_OUT_BIN, CAUGHT, ROLL}
+var state: State
 
 func _ready() -> void:
 	Gamemanager.player = self
 	update_camera()
 	animation_tree.active = true
+	_enter_state(State.IDLE)
+
+func _enter_state(new_state: State) -> void:
+	state = new_state
+
+func _update_agent_target() -> void:
+	match state:
+		State.IDLE:
+			animation_tree["parameters/conditions/is_idle"] = true
+			animation_tree["parameters/conditions/is_moving"] = false
+			state_idle()
+		State.MOVE:
+			animation_tree["parameters/conditions/is_idle"] = false
+			animation_tree["parameters/conditions/is_moving"] = true
+			state_move()
+		State.CAUGHT:
+			animation_tree["parameters/conditions/is_caught"] = true
+		State.MOVE_IN_BIN:
+			animation_tree["parameters/conditions/is_jumped"] = true
+			animation_tree["parameters/conditions/is_idle"] = false
+			animation_tree["parameters/conditions/is_moving"] = false
+			can_move = false
+		State.MOVE_OUT_BIN:
+			animation_tree["parameters/conditions/is_jumped_out"] = true
+			animation_tree["parameters/conditions/is_jumped"] = false
+			animation_tree["parameters/conditions/is_idle"] = false
+			animation_tree["parameters/conditions/is_moving"] = false
+		State.ROLL:
+			animation_tree["parameters/conditions/is_idle"] = false
+			animation_tree["parameters/conditions/is_moving"] = false
+			animation_tree["parameters/conditions/roll"] = true
 
 func update_camera():
 	if Gamemanager.cur_cam_node:
@@ -27,6 +59,7 @@ func update_camera():
 
 func _physics_process(delta: float) -> void:
 	update_camera()
+	_update_agent_target()
 	if can_move:
 		cam_input_direction = Vector2.ZERO
 		var raw_input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -41,41 +74,21 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 	else:
 		velocity = Vector3.ZERO
-	update_animation_paramters()
 
-	#var walking = true
-	#if anim.assigned_animation == "barrel_roll" or anim.assigned_animation == "caught":
-		#walking = false
-		#if anim.is_playing() == false:
-			#walking = true
-	#if velocity.length() > 0 and walking:
-		#get_node("raccoon/AnimationPlayer").play("walk_animation")
-	#elif velocity.length() <= 0 or !walking:
-		#get_node("raccoon/AnimationPlayer").clear_queue()
-	
-func update_animation_paramters():
-	if (velocity == Vector3.ZERO):
-		animation_tree["parameters/conditions/is_idle"] = true
-		animation_tree["parameters/conditions/is_moving"] = false
-	else:
-		animation_tree["parameters/conditions/is_idle"] = false
-		animation_tree["parameters/conditions/is_moving"] = true
-		exited_bin = false
-	if (Input.is_action_just_pressed("barrel_roll")):
-		animation_tree["parameters/conditions/roll"] = true
-	else:
-		animation_tree["parameters/conditions/roll"] = false
-	if is_in_bin:
-		animation_tree["parameters/conditions/is_jumped"] = true
-	if exited_bin:
-		animation_tree["parameters/conditions/is_jumped"] = false
-		animation_tree["parameters/conditions/is_jumped_out"] = true
-	if exited_bin and animation_tree["parameters/conditions/is_moving"]:
-		animation_tree["parameters/conditions/is_jumped_out"] = false
+func state_move():
+	if velocity == Vector3.ZERO:
+		_enter_state(State.IDLE)
 
-		
-		
-		
+func state_idle():
+	if velocity > Vector3.ZERO:
+		_enter_state(State.MOVE)
+
+func _on_player_in_bin():
+	if state == State.IN_BIN:
+		_enter_state(State.MOVE_OUT_BIN)
+	else:
+		_enter_state(State.MOVE_IN_BIN)
+
 func _input(event):
 	if event.is_action_pressed("barrel_roll"):
 		barrel_roll()
@@ -85,24 +98,30 @@ func _input(event):
 func game_won():
 		if Gamemanager.prog_bar_nmb >= 6:
 			get_tree().change_scene_to_file("res://scenes/mission_accomplished.tscn")
-		else:
-			print(Gamemanager.prog_bar_nmb)
 
 func game_over():
-	print("over")
-	animation_tree["parameters/conditions/is_caught"] = true
 	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://scenes/game_over.tscn")
-	print(Gamemanager.prog_bar_nmb)
 
 func barrel_roll():
-	#anim.play("barrel_roll")
+	_enter_state(State.ROLL)
 	move_speed = 50
-	await get_tree().create_timer(0.5).timeout
-	move_speed = 5.0
 	
 func reset():
 	self.position = Vector3(-157.159,0,32.693)
 	Gamemanager.cur_cam_node = Gamemanager.reset_cam
 	Gamemanager.reset_cam.reset_make_current()
 	print(Gamemanager.cur_cam_node, Gamemanager.reset_cam)
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "jump_001":
+		_enter_state(State.IN_BIN)
+	if anim_name == "jump_out":
+		animation_tree["parameters/conditions/is_jumped_out"] = false
+		can_move = true
+		_enter_state(State.IDLE)
+	if anim_name == "barrel_roll":
+		animation_tree["parameters/conditions/roll"] = false
+		move_speed = 5.0
+		print("roll end")

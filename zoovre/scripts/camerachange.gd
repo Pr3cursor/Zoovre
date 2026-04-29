fdbf4d52ca2542ba1c9cf66208d828f6760a40d6
Luca Mahncke <scooper@posteo.de>
2026-04-29 21:58:58 +0200

    Camera change fix (Big P)
    Pray to the machine spirit

--- zoovre/scripts/camerachange.gd
+++ zoovre/scripts/camerachange.gd
@@ -1,58 +1,28 @@
 extends Node3D
 
-
-#@warning_ignore("unused_signal")
-#signal sig_change_camera(prev_cam_id, next_cam_id)
-
-#@export var prev_cam_id: int
-#@export var next_cam_id: int
-
 @export var prev_cam: SurveillanceCam
 @export var next_cam: SurveillanceCam
 
-var current_cam_id: int = 1
-var switch = true
+var locked: bool = false
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body.is_in_group("player"):
-		if switch:
-			next_cam.camera.make_current()
-			prev_cam.camera.current = false
-			Gamemanager.cur_cam_node = next_cam
-			Gamemanager.auto_cur_cam_id = next_cam.camera_id
-		else:
-			prev_cam.camera.make_current()
-			next_cam.camera.current = false
-			Gamemanager.cur_cam_node = prev_cam
-			Gamemanager.auto_cur_cam_id = prev_cam.camera_id
-		switch = !switch
- 
+	if not body.is_in_group("player"):
+		return
+	
+	if locked:
+		return
+	
+	locked = true
 
+	if Gamemanager.cur_cam_node == prev_cam:
+		next_cam.camera.make_current()
+		Gamemanager.cur_cam_node = next_cam
+		Gamemanager.auto_cur_cam_id = next_cam.camera_id
 
-	#print("hi")
-	#if body.is_in_group("player"):
-		#if switch:
-			##print("prev ", prev_cam_id)
-			##print("next: ", next_cam_id)
-			#sig_change_camera.emit(prev_cam_id, next_cam_id)
-			#switch = false
-		#else:
-			##print("prev ", next_cam_id)
-			##print("next: ", prev_cam_id)
-			#sig_change_camera.emit(next_cam_id,prev_cam_id)
-			#switch = true
+	elif Gamemanager.cur_cam_node == next_cam:
+		prev_cam.camera.make_current()
+		Gamemanager.cur_cam_node = prev_cam
+		Gamemanager.auto_cur_cam_id = prev_cam.camera_id
 
-
-#func _on_body_entered(body: Node3D) -> void:
-	#if body.is_in_group("player"):
-		##print(body)
-		#sig_change_camera.emit(old_cam_id,new_cam_id,current_cam_id)
-
-
-#func _on_area_3d_body_entered(body: Node3D) -> void:
-	#if body.is_in_group("player")and Gamemanager.auto_cur_cam_id == old_cam_id:
-		#sig_change_camera.emit(new_cam_id,current_cam_id)
-#
-#func _on_area_3d_2_body_entered(body: Node3D) -> void:
-	#if body.is_in_group("player") and Gamemanager.auto_cur_cam_id == new_cam_id:
-		#sig_change_camera.emit(old_cam_id,current_cam_id)
+	await get_tree().create_timer(0.15).timeout
+	locked = false
